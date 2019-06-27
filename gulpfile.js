9cabc9f4ea95b4de7072b39f3445d075b9eb237f
Ravinsky Yuriy <ravinskyyuriy@gmail.com>
2019-06-27 11:04:02 +0300

    setup-respond

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ sass.compiler = require('node-sass');
 
 //Tasks functions
 const compile_scss = ()=>{
-    return gulp.src("./src/sass/**/*.scss")
+    return gulp.src("./src/sass/main.scss")
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./build/css'))
 }
